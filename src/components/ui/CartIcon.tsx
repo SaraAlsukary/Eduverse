ee43dcb7d2099b995b141654e90085d5aac4e7b9
SaraAlsukary <sarals6216@gmail.com>
2025-12-25 20:11:12 +0300

    commit

--- src/components/ui/CartIcon.tsx
+++ src/components/ui/CartIcon.tsx
@@ -5,7 +5,7 @@ const CartIcon = ({ totalCourse }: { totalCourse: number }) => {
     const navigate = useNavigate()
     return (
         <div className="flex justify-end relative">
-            <HiMiniShoppingCart size={40} className="text-purple-700 cursor-pointer" onClick={() => navigate('cart')} />
+            <HiMiniShoppingCart size={40} className="text-purple-700 cursor-pointer" onClick={() => navigate('/home/cart')} />
             {totalCourse ? <span className="bg-blue-700 h-5 w-5 rounded-full text-white flex items-center justify-center absolute right-2">{totalCourse}</span>
                 : ""}</div>
     )
